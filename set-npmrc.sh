b89e789a3d5ae8a6749f63045292edab9575c7a1
collin.lucke@gmail.com <collin.lucke@gmail.com>
2024-11-17 20:23:53 -0700

    ???????????????????????????? 6

--- set-npmrc.sh
+++
@@ -1,4 +0,0 @@
-echo "registry=https://registry.npmjs.org/" > .npmrc
-echo "@collinlucke:registry=https://npm.pkg.github.com" >> .npmrc
-echo "//npm.pkg.github.com/:_authToken=${GIT_REGISTRY_TOKEN}" >> .npmrc
-
